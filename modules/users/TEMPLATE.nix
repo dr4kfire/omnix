f3ee84e4eedff569263bf6129b941e0e0dde74c2
catnip <147838547+dr4kfire@users.noreply.github.com>
2026-04-13 12:07:48 +0200

    Work: Added defaults to the TEMPLATE.nix user file

---
+++ modules/users/TEMPLATE.nix
@@ -0,0 +1,14 @@
+{
+  config.var = {
+    # CHANGEME
+    username = "user";
+    pfp = "";
+
+    keyboardLayout = "pl";
+
+    git = {
+      username = "user";
+      email = "example@email.com";
+    };
+  };
+}
