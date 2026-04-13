7003afd5d8fa18cbd1243112ca7670052ffade54
catnip <147838547+dr4kfire@users.noreply.github.com>
2026-04-13 16:47:24 +0200

    Work: config-merge.nix implemented

--- omnix/config-merge.nix
+++ omnix/config-merge.nix
@@ -1,17 +1,34 @@
+#
+# This file parses configs in ./hosts
+#
 {
-  config,
   nixpkgs,
   home-manager,
   ...
 }: let
-  # Fetches files and directories from ./hosts and checks if a configuration.nix
-  # file exists inside of them
+  # List of names of directories that are valid hosts configs
   avaliableHosts = builtins.filter (
     content_name:
       builtins.pathExists ./hosts + "/" + content_name + "/configuration.nix"
   ) (builtins.attrNames (builtins.readDir ./hosts));
-
-  pkgs = import nixpkgs {inherit system;};
-  inherit (pkgs) lib;
 in {
+  nixosConfigurations = let
+    hosts_map = host: let
+      host_conf = import ./hosts/${host}/configuration.nix;
+      system = host_conf.config.host.system;
+      pkgs = import nixpkgs {inherit system;};
+    in [
+      {
+        name = host;
+        value = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            (import home-manager.nixosModule)
+          ];
+          specialArgs = {inherit pkgs;};
+        };
+      }
+    ];
+  in
+    builtins.listToAttrs builtins.concatMap hosts_map avaliableHosts;
 }
