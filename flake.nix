0e6d3cfeba49a5a928887627a2abf6d717b7d6f6
catnip <147838547+dr4kfire@users.noreply.github.com>
2026-04-13 10:57:22 +0200

    Added: Basic flake.nix structure

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "" "
+    OMNix is an all-in-one flake based configuration for NixOS based systems!
+    It provides basic tools for customization, default configs and works out of the box!
+    " "";
+
+  inputs = {
+  };
+
+  outputs = {
+  };
+}
