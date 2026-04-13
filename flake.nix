b7cf35656dfece8411cfd790328b17c113d0dd1e
catnip <147838547+dr4kfire@users.noreply.github.com>
2026-04-13 20:06:53 +0200

    Fix: Fixed parenthesis in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
-  description = "" "
+  description = "
     OMNix is an all-in-one flake based configuration for NixOS based systems!
     It provides basic tools for customization, default configs and works out of the box!
-    " "";
+    ";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
