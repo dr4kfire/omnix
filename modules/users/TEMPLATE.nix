d010f10f29c3fd1bd2ffaa29c4c9253b61a972f2
catnip <147838547+dr4kfire@users.noreply.github.com>
2026-04-14 13:26:49 +0200

    Work: Basic cleanup of the TEMPLATE.nix user file

--- modules/users/TEMPLATE.nix
+++ modules/users/TEMPLATE.nix
@@ -1,17 +1,15 @@
 {
   # Copy this file and change its contents to add
   # a new user to this config
-  config.var = {
+  username = "user";
+  defaultPassword = "password";
+
+  groups = ["wheel"]; # wheel is sudo
+
+  keyboardLayout = "pl";
+
+  git = {
     username = "user";
-    defaultPassword = "password";
-
-    groups = ["wheel"]; # wheel is sudo
-
-    keyboardLayout = "pl";
-
-    git = {
-      username = "user";
-      email = "example@email.com";
-    };
+    email = "example@email.com";
   };
 }
